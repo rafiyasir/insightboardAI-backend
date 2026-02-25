07f34561fb60ceaa17c9ca897447acecdd675964
rafiuzzaman202 <rafiuzzaman202@gmail.com>
2026-02-25 16:24:16 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from "express";
+import cors from "cors";
 import taskRoute from "./routes/task.routes";
 
 export function createApp() {
   const app = express();
+
+  const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+
+  app.use(
+    cors({
+      origin: allowedOrigins,
+      credentials: true,
+    }),
+  );
 
   app.use(express.json());
   app.use(express.text());
@@ -11,7 +21,7 @@ export function createApp() {
     res.status(200).json({ message: "insighboardAI is running" });
   });
 
-  app.use('/api',taskRoute)
+  app.use("/api", taskRoute);
 
   return app;
 }
